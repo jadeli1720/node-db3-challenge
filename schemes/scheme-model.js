828ed66a0c23af3f4a217059074a59a0a26e990a
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-11 16:23:07 -0600

    findbyid and find steps work

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,14 +2,55 @@ const db = require('../data/db-config')
 
 module.exports = {
     find,
-    // findById,
-    // findSteps,
-    // add,
-    // update,
-    // remove
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
 }
 
+// GOTCHA!!! DON'T FORGET TO RETURN!!!!!!
+
 function find() {
+    //select * from schemes
     return db('schemes')
 };
+
+function findById(id) {
+    //select * from schemes where id = 123
+    return db('schemes')
+        .where(({ id }))
+        .first()
+};
+
+function findSteps(id) {
+    /*
+    select * Steps as St
+    join Scheme as sc on sc.id = st.scheme_id
+    where st.id = id
+    */
+   return db('steps as st')
+            .join('schemes as sc', 'sc.id', 'st.scheme_id')
+            .select(
+                'st.id', 
+                'sc.scheme_name', 
+                'st.step_number',
+                'st.instructions'
+                )
+            .where({"sc.id": id})
+
+};
+
+
+function add() {
+
+};
+
+function update() {
+
+};
+
+function remove() {
+
+}
 
